63e814e7ded89ca61c0bcc2ab8daf7b5da2170a5
mohammad khakshoor <mkhbeats@gmail.com>
2023-08-28 11:35:37 +0330

    REFACTOR: accumulate all the btns styles in the one class style in tailwind

--- src/sections/AllButtons.jsx
+++ src/sections/AllButtons.jsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
+
 const AllButtons = ({ calc, setCalc }) => {
+    // const [resetNum, setResetNum] = useState(false);
+    console.log(calc);
     function handlePassNumber(e) {
+        const lastChar = calc.at?.(length - 1);
         if (e.target.innerHTML === "DEL") {
-            const deleteLastValue = calc?.split?.("");
-            deleteLastValue?.pop();
-            return setCalc?.(deleteLastValue?.join?.(""));
+            // if (!calc.split?.("")) {
+            //     return;
+            // }
+            const deleteLastValue = calc.split("");
+            deleteLastValue.pop();
+            return setCalc(deleteLastValue.join(""));
         }
         if (e.target.innerHTML === "Reset") {
             return setCalc("");
         }
         if (e.target.innerHTML === "=") {
-            const num = eval(calc);
+            // setResetNum(true);
+            const num = String(eval(calc));
             return setCalc(() => num);
         }
 
         if (e.target.tagName === "BUTTON") {
+            if (
+                lastChar === "*" ||
+                lastChar === "/" ||
+                lastChar === "+" ||
+                lastChar === "-" ||
+                lastChar === "."
+            ) {
+                if (
+                    e.target.innerHTML === "*" ||
+                    e.target.innerHTML === "/" ||
+                    e.target.innerHTML === "+" ||
+                    e.target.innerHTML === "-" ||
+                    e.target.innerHTML === "."
+                ) {
+                    return;
+                }
+            }
+            // if (resetNum) {
+            //     setCalc("");
+            //     setResetNum(false);
+            // }
             setCalc((prevValue) => prevValue + e.target.innerHTML);
         }
     }
@@ -22,60 +52,24 @@ const AllButtons = ({ calc, setCalc }) => {
             onClick={handlePassNumber}
             className="-bg-dark-blue rounded-xl font-Poppins grid grid-cols-4 grid-rows-5 justify-items-stretch gap-3 p-7"
         >
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold hover:-bg-redish-orange transition-all ">
-                7
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                8
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                9
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:bg-red-600 transition-all ">
-                DEL
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                4
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                5
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                6
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:bg-yellow-500 transition-all ">
-                +
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                1
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                2
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                3
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:bg-yellow-500 transition-all ">
-                -
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                .
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:-bg-redish-orange transition-all ">
-                0
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:bg-yellow-500 transition-all ">
-                /
-            </button>
-            <button className="-bg-milky flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:bg-yellow-500 transition-all ">
-                *
-            </button>
-            <button className="-bg-milky col-span-2 flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:bg-gray-400 transition-all ">
-                Reset
-            </button>
-            <button className="-bg-milky col-span-2 flex  min-h-[4.5rem] items-center justify-center rounded-md text-3xl font-bold  hover:bg-green-500 transition-all ">
-                =
-            </button>
+            <button className="btns ">7</button>
+            <button className="btns ">8</button>
+            <button className="btns ">9</button>
+            <button className="btns  hover:bg-red-600  ">DEL</button>
+            <button className="btns ">4</button>
+            <button className="btns ">5</button>
+            <button className="btns ">6</button>
+            <button className="btns hover:bg-yellow-400 ">+</button>
+            <button className="btns ">1</button>
+            <button className="btns ">2</button>
+            <button className="btns ">3</button>
+            <button className="btns hover:bg-yellow-400 ">-</button>
+            <button className="btns ">.</button>
+            <button className="btns ">0</button>
+            <button className="btns hover:bg-yellow-400 ">/</button>
+            <button className="btns hover:bg-yellow-400 ">*</button>
+            <button className="btns col-span-2  hover:bg-gray-400">Reset</button>
+            <button className="btns col-span-2 hover:bg-green-500">=</button>
         </div>
     );
 };
