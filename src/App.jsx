a83a9fcc65734ac0bd726c946831b0277e45f76f
mohammad khakshoor <mkhbeats@gmail.com>
2023-08-28 11:36:28 +0330

    CREATE: add some computing validation to calculator logic

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import { useState } from "react";
 const App = () => {
     const [calc, setCalc] = useState("");
     return (
-        <main className="-bg-main-blue min-h-screen">
-            <div className="max-container flex flex-col justify-between sm:justify-normal min-h-screen sm:grid sm:place-items-center  ">
+        <main className="-bg-main-blue min-h  ">
+            <div className="max-container flex flex-col justify-between sm:justify-normal min-h sm:grid sm:place-items-center  ">
                 <Header />
-                <section className="  contain-margin my-10  sm:mx-auto sm:w-[35rem] ">
+                <section className="  contain-margin my-[max(2.5rem,0%)]   sm:mx-auto sm:w-[35rem] ">
                     <InputNumber calc={calc} setCalc={setCalc} />
                     <AllButtons calc={calc} setCalc={setCalc} />
                 </section>
